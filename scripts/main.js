68efd5124eadfde24e8b993229ee7bf43d4389b1
Anna Abbott <aabbott@byupathway.edu>
2025-06-06 21:19:58 -0500

    Navigation working home page

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const NavOpenBtn = document.querySelector("#NavOpenBtn");
+const NavHamburgerBtn = document.querySelector("#NavHamburgerBtn");
+const sideNav = document.querySelector("#sideNav");
+
+NavOpenBtn.addEventListener("click", () => {
+  NavHamburgerBtn.classList.toggle("hide");
+  sideNav.classList.toggle("open");
+  NavOpenBtn.classList.toggle("open");
+});
+
+NavHamburgerBtn.addEventListener("click", () => {
+  NavHamburgerBtn.classList.toggle("hide");
+  console.log("clicked to open");
+  sideNav.classList.toggle("open");
+  NavOpenBtn.classList.toggle("open");
+});
