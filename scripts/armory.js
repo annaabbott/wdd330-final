7749d513b8c9a87d6d07130e50ee4799910b5dc9
Anna Abbott <aabbott@byupathway.edu>
2025-06-09 13:50:44 -0500

    armor cards created, displayed

--- scripts/armory.js
+++ scripts/armory.js
@@ -7,9 +7,31 @@ async function getData() {
     }
     const json = await response.json();
     console.log(json);
+    return json;
   } catch (error) {
     console.error(error.message);
   }
 }
+async function renderArmory() {
+  const armorData = await getData();
+  armorData.results.forEach((armorElement) => {
+    createArmorCard(armorElement);
+  });
+}
 
-getData();
+function createArmorCard(armorData) {
+  const armorDiv = document.querySelector("#armorContainer");
+  const armorCard = document.createElement("div");
+  armorDiv.appendChild(armorCard);
+  const armorName = document.createElement("h2");
+  armorName.innerText = armorData.name;
+  armorCard.appendChild(armorName);
+  const armorCategory = document.createElement("p");
+  armorCategory.innerText = `Type: ${armorData.category}`;
+  armorCard.appendChild(armorCategory);
+  const armorClass = document.createElement("p");
+  armorClass.innerText = `Armor Class (AC): ${armorData.ac_display}`;
+  armorCard.appendChild(armorClass);
+}
+
+renderArmory();
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+// import { getData } from "armory.js";
+
 const NavOpenBtn = document.querySelector("#NavOpenBtn");
 const NavHamburgerBtn = document.querySelector("#NavHamburgerBtn");
 const sideNav = document.querySelector("#sideNav");
