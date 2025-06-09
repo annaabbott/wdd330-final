c6e7b45f8cec36de1f93d8590021dab2e853bd86
Anna Abbott <aabbott@byupathway.edu>
2025-06-09 14:57:35 -0500

    weapon cards created and displayed

--- scripts/armory.js
+++ scripts/armory.js
@@ -1,5 +1,5 @@
-async function getData() {
-  const url = "https://api.open5e.com/v2/armor/?format=json";
+async function getData(url) {
+  //   const url = "https://api.open5e.com/v2/armor/?format=json";
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -13,9 +13,16 @@ async function getData() {
   }
 }
 async function renderArmory() {
-  const armorData = await getData();
+  const armorURL = "https://api.open5e.com/v2/armor/?format=json";
+  const armorData = await getData(armorURL);
   armorData.results.forEach((armorElement) => {
     createArmorCard(armorElement);
+  });
+
+  const weaponsURL = "https://api.open5e.com/v2/weapons/?format=json";
+  const weaponsData = await getData(weaponsURL);
+  weaponsData.results.forEach((weaponElement) => {
+    createWeponCard(weaponElement);
   });
 }
 
@@ -32,6 +39,26 @@ function createArmorCard(armorData) {
   const armorClass = document.createElement("p");
   armorClass.innerText = `Armor Class (AC): ${armorData.ac_display}`;
   armorCard.appendChild(armorClass);
+}
+
+function createWeponCard(weaponsData) {
+  const weaponDiv = document.querySelector("#weaponContainer");
+  const weaponCard = document.createElement("div");
+  weaponDiv.appendChild(weaponCard);
+  const weaponName = document.createElement("h2");
+  weaponName.innerText = weaponsData.name;
+  weaponCard.appendChild(weaponName);
+  const weaponDesc = document.createElement("p");
+  weaponDesc.innerText = `Damage: ${weaponsData.damage_dice}`;
+  weaponCard.appendChild(weaponDesc);
+  const weaponProperties = document.createElement("p");
+  weaponProperties.innerText = `Properties: ${weaponsData.properties.join(
+    ", "
+  )}`;
+  weaponCard.appendChild(weaponProperties);
+  const weaponReach = document.createElement("p");
+  weaponReach.innerText = `Range: ${weaponsData.range}; Reach: ${weaponsData.reach}`;
+  weaponCard.appendChild(weaponReach);
 }
 
 renderArmory();
